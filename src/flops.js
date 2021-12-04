77040b5a41431e9f86701aa99f9a54bd0279d8d9
jonhayjr <jonhayjr@gmail.com>
2021-12-04 12:19:32 -0800

    Intialize Repo

---
+++ src/flops.js
@@ -0,0 +1,26 @@
+export const flops = [
+{   
+    id: 1,
+    title: 'The Alamo',
+    year: 2004,
+    imageSrc: ''
+},
+{   
+    id: 2,
+    title: 'Windtalkers',
+    year: 2002,
+    imageSrc: ''
+},
+{   
+    id: 3,
+    title: 'Around the World in 80 days',
+    year: 2004,
+    imageSrc: ''
+},
+{   
+    id: 4,
+    title: 'Stealth',
+    year: 2005,
+    imageSrc: ''
+}
+]
