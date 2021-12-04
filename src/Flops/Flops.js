297b4e890ed0434fe0d5fbbc4aa2c9bb11be15b5
jonhayjr <jonhayjr@gmail.com>
2021-12-04 13:41:43 -0800

    Added additional styling to flop cards

--- src/Flops/Flops.js
+++ src/Flops/Flops.js
@@ -1,14 +1,26 @@
+import {useState} from 'react';
+
 import flopsData from '../flopsData';
 
+import './Flops.css';
+
 export const Flops = () => {
+    const [flops, setFlops] = useState(flopsData);
+    const [selected, setSelected] = useState(1);
+
+    const handleClick = (event) => {
+        console.log(event.target.id)
+    }
+
     return (
         <section className="flops-grid">
             {
-                flopsData.map(flop => {
-                   return (<div key={flop.id}>
+                flops.map(flop => {
+                   return (<div key={flop.id} className="card">
                                 <h1>{flop.title}</h1>
                                 <p>{flop.year}</p>
                                 <img src={flop.imageSrc} alt={flop.title}/>
+                                <button className="btn" disabled={flop.id === selected ? false : true} onClick={(e) => {handleClick(e)}}>{flop.id === selected ? 'Unvote' : 'Vote'}</button>
                             </div>)
                 })
             }
--- src/flopsData.js
+++ src/flopsData.js
@@ -3,25 +3,29 @@ const flopsData = [
     id: 1,
     title: 'The Alamo',
     year: 2004,
-    imageSrc: '../../img/TheAlamo.jpeg'
+    imageSrc: '../../img/TheAlamo.jpeg',
+    isSelected: true
 },
 {   
     id: 2,
     title: 'Windtalkers',
     year: 2002,
-    imageSrc: '../../img/Windtalkers.jpeg'
+    imageSrc: '../../img/Windtalkers.jpeg',
+    isSelected: true
 },
 {   
     id: 3,
-    title: 'Around the World in 80 days',
+    title: 'Around the World in 80 Days',
     year: 2004,
-    imageSrc: '../../img/AroundTheWorldIn80Days.jpeg'
+    imageSrc: '../../img/AroundTheWorldIn80Days.jpeg',
+    isSelected: true
 },
 {   
     id: 4,
     title: 'Stealth',
     year: 2005,
-    imageSrc: '../../img/Stealth.jpeg'
+    imageSrc: '../../img/Stealth.jpeg',
+    isSelected: true
 }
 ]
 
